4fe071e9ee9d96e16ba72c0847a5294d5fb7a98c
Leon <Leon>
2016-03-20 16:18:03 +0100

    Added Location Tracker -ToDO Link gpsTracker activity to main

--- Zeiterfassung/src/de/praxisModul/zeiterfassung/MainActivity.java
+++ Zeiterfassung/src/de/praxisModul/zeiterfassung/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView tv = new TextView(this);
+		tv.setText("Hello Android");
+		setContentView(tv);
 	}
 
 	@Override
